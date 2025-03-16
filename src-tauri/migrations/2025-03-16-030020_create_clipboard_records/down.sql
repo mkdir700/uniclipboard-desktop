0e235ef1636449589429909752b8486d22b5da9b
mkdir700 <mkdir700@gmail.com>
2025-03-16 21:56:59 +0800

    :sparkles: 新增 ClipboardRecord

---
+++ src-tauri/migrations/2025-03-16-030020_create_clipboard_records/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE clipboard_records;
+DROP TRIGGER update_clipboard_records_updated_at;
--- src-tauri/src/schema.rs
+++ src-tauri/src/schema.rs
@@ -1,6 +1,19 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    clipboard_records (id) {
+        id -> Text,
+        device_id -> Text,
+        remote_file_url -> Nullable<Text>,
+        local_file_url -> Nullable<Text>,
+        content_type -> Text,
+        is_favorited -> Bool,
+        created_at -> Integer,
+        updated_at -> Integer,
+    }
+}
+
+diesel::table! {
     devices (id) {
         id -> Text,
         ip -> Nullable<Text>,
@@ -11,3 +24,8 @@ diesel::table! {
         updated_at -> Integer,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    clipboard_records,
+    devices,
+);
